0df6db6b3b491ba3ffb0648f291eac71be9b485b
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-12-02 17:09:02 +1100

    added todolist logic for adding and removing todos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <!-- The main container of our app -->
+    <h1>My Todo App</h1>
+
+    <!-- Input field for adding new todos -->
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add new todo" />
+
+    <!-- Component responsible for displaying the list of todos -->
+    <TodoList :todos="todos" @remove="removeTodo" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<!--template section contains the HTML structure of the component.--->
+<!--<input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add new todo" /> 
+  is an input field bound to the newTodo variable using v-model. -->
+<!--<TodoList :todos="todos" @remove="removeTodo" /> is our TodoList component. 
+  It receives the todos array as a prop and listens for the remove event to handle todo removal.-->
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import TodoList from "./components/TodoList.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TodoList,
+  },
+  setup() {
+    const newTodo = ref('');
+    const todos = ref<string[]>([]);
+
+    function addTodo() {
+      if (newTodo.value.trim() !== '') {
+        todos.value.push(newTodo.value);
+        newTodo.value = '';
+      }
+    }
+
+    function removeTodo(index: number) {
+      todos.value.splice(index, 1);
+    }
+
+    return {
+      newTodo,
+      todos,
+      addTodo,
+      removeTodo,
+    };
+  },
+});
+</script>
+
+<!--The <script> section contains the logic of the component written in TypeScript.
+defineComponent is used to define the Vue component.
+import statements bring in necessary functionality. For instance, we import TodoList from its file.
+setup() is a composition API function where we define our component's reactive data and methods.
+ref() creates reactive variables (newTodo and todos) that can trigger reactivity when their values change.
+addTodo() is a function that adds a new todo to the todos array if the input isn't empty.
+removeTodo() is a function that removes a todo item from the todos array based on its index.-->
---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="remove(index)">❌</button>
+      </li>
+    </ul>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+    name: 'TodoList',
+    props: {
+      todos: {
+        type: Array as PropType<string[]>,
+        required: true,
+      },
+    },
+    emits: ['remove'],
+    methods: {
+      remove(index: number) {
+        this.$emit('remove', index);
+      },
+    },
+  });
+  </script>
+  
